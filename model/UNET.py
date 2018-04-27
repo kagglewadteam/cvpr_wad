e2e5c411acc5a83dfe8f873111dcd8573d65cc6f
yushinliu <yuxinliu_oliver@outlook.com>
2018-04-27 16:53:41 +0200

    modified generator

--- model/UNET.py
+++ model/UNET.py
@@ -12,6 +12,7 @@ import keras.preprocessing.image as KPImage
 from PIL import Image
 from skimage.io import imread
 from keras.backend import tensorflow_backend as KTF
+from gen_vis import TensorBoardWrapper
 # internal import
 from utils.loss import *
 
@@ -37,11 +38,14 @@ def train_generator(image_datagen, mask_datagen, batch_size):
     seed = 1
     image_flow = image_datagen.flow_from_directory('/home/liuyn/masterthesis/kaggle_sampledata/train_color', batch_size=batch_size, target_size=(384, 384), color_mode="rgb", class_mode=None, seed=seed)
     label_flow = mask_datagen.flow_from_directory('/home/liuyn/masterthesis/kaggle_sampledata/train_label', batch_size=batch_size, target_size=(384, 384), color_mode="grayscale", class_mode=None, seed=seed)
-
+	return zip(image_flow,label_flow)
+	'''
     while True:
         image_batch = next(image_flow)
         label_batch = next(label_flow)
         yield image_batch, label_batch
+	'''
+
 
 
 # flow from validation directory
@@ -49,10 +53,13 @@ def val_generator(image_datagen, mask_datagen, batch_size):
     seed = 1
     image_flow = image_datagen.flow_from_directory('/home/liuyn/masterthesis/kaggle_sampledata/val_color', batch_size=batch_size, target_size=(384, 384), color_mode="rgb", class_mode=None, seed=seed)
     label_flow = mask_datagen.flow_from_directory('/home/liuyn/masterthesis/kaggle_sampledata/val_label', batch_size=batch_size, target_size=(384, 384), color_mode="grayscale", class_mode=None, seed=seed)
+	return zip(image_flow,label_flow)
+	'''
     while True:
         image_batch = next(image_flow)
         label_batch = next(label_flow)
         yield image_batch, label_batch
+	'''
 
 
 def create_model():
@@ -119,7 +126,9 @@ def train(cfg, train_generator, val_generator):
     callback_list = [EarlyStopping(monitor='val_loss', patience=10, verbose=1)]
     callback_list.append(ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=0, verbose=1))
     callback_list.append(ModelCheckpoint(weights_file, monitor='val_loss', verbose=1, period=1, save_best_only=True, save_weights_only=True))
-    callback_list.append(TensorBoard(log_dir='./tensorboard/log', write_images=True, histogram_freq=1,write_images=True))  #visualize model 
+	#callback_list.append(TensorBoard(log_dir='./tensorboard/log', write_images=True, histogram_freq=0,write_images=True))
+    callback_list.append(TensorBoardWarpper(val_generator, nb_steps=1, log_dir='./tensorboard/log', histogram_freq=1,
+                               batch_size=cfg['batch_size'], write_graph=True, write_grads=True))  #visualize model 
     # train model
     model.fit_generator(train_generator,
                         steps_per_epoch=2048 // cfg['batch_size'],
@@ -151,3 +160,4 @@ def run(cfg):
 
     # train model
     train(cfg, train_generator(image_datagen, mask_datagen, cfg['batch_size']), val_generator(image_datagen, mask_datagen, cfg['batch_size']))
+
